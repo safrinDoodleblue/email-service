b988a99a41ae5021293aa0f3355cefe46d500e66
safrinDoodleblue <safrin.doodleblue@gmail.com>
2025-07-22 19:04:34 +0530

    connected with user-service for session based process

--- server.js
+++ server.js
@@ -1,86 +1,31 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const passport = require('passport');
-require('dotenv').config();
-require('./config/passport');
+const dotenv = require('dotenv');
+const { sendWelcomeEmail } = require('./utils/emailService');
 
-const routes = require('../../Routes');
+
+dotenv.config();
 
 const app = express();
-
 app.use(express.json());
 
+app.post('/api/send', async (req, res) => {
+  const { to, name } = req.body;
 
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET, 
-    resave: false,//Avoids unnecessary saving if session didn't change
-    saveUninitialized: false,//	Avoids saving empty sessions
-    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
-    cookie: { maxAge: 1000 * 60 * 60 }, 
-  })
-);
+  if (!to || !name) {
+    return res.status(400).json({ error: 'Missing "to" or "name" in request' });
+  }
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-app.use('/api', routes);
-
-const PORT = process.env.PORT || 3000;
-
-const startServer=async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-         console.log('MongoDB connected');
-        app.listen(PORT,()=>{
-            console.log(`Server running at http://localhost:${process.env.PORT}`);
-        })
-    } catch (err) {
-        console.error('MongoDB connection error:', err);
-    }
-};
-startServer();
+  try {
+    await sendWelcomeEmail(to, name);
+    res.json({ message: 'Welcome email sent successfully' });
+  } catch (err) {
+    console.error('Error sending email:', err);
+    res.status(500).json({ error: 'Failed to send email' });
+  }
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const express=require('express');
-// const mongoose=require('mongoose');
-// const passport = require('passport');
-// require('dotenv').config();
-// require('./config/passport');
-// const routes=require('./Routes')
-
-// const app=express();
-
-// app.use(express.json());
-// app.use(passport.initialize());
-// const PORT=process.env.PORT ||3000;
-// app.use('/api',routes);
-
-// const startServer=async () => {
-//     try {
-//         await mongoose.connect(process.env.MONGO_URI);
-//          console.log('MongoDB connected');
-//         app.listen(PORT,()=>{
-//             console.log(`Server running at http://localhost:${process.env.PORT}`);
-//         })
-//     } catch (err) {
-//         console.error('MongoDB connection error:', err);
-//     }
-// };
-// startServer();
+const PORT = process.env.PORT || 7000;
+app.listen(PORT, () => {
+  console.log(`Email service running on port ${PORT}`);
+});
